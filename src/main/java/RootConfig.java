55d1780550b50beeaa2435a37fdb55d374213694
TheCrunchy <crunchycamsta@gmail.com>
2018-10-12 17:27:44 +0100

    Initial commit

---
+++ src/main/java/RootConfig.java
@@ -0,0 +1,18 @@
+
+
+import ninja.leaping.configurate.objectmapping.Setting;
+import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ConfigSerializable
+public class RootConfig {
+
+    @Setting("toggled")
+    private List<Toggled> toggled = new ArrayList<>();
+
+    public List<Toggled> getCategories() {
+        return toggled;
+    }
+}
